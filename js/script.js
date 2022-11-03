c6fc65309e8687d363eb7054c254d52b6eb5a052
unknown <nduflot@dawan.fr>
2022-11-03 11:49:25 +0100

    storage : cookie mini-exo cookie

--- js/script.js
+++ js/script.js
@@ -14,3 +14,30 @@ function qS(selector){
 function qSAll(selector){
     return document.querySelectorAll(selector);
 }
+
+/* création de cookie */
+/**
+ * @param {string} name
+ * @param {string} value
+ * @param {number} days
+ */
+function setCookie(name, value = '', days = -1){
+    let maxAge = days * 24 * 60 * 60;
+    document.cookie = `${name}=${value}; max-age=${maxAge}; Samesite=Strict; Secure`;
+}
+
+/* récupérer un cookie s'il existe */
+/**
+ * @param {string} name 
+ */
+function getCookie(name){
+    let tabCookie = document.cookie.split('; ');
+    console.log(tabCookie);
+    for(cookie of tabCookie){
+        let tabValue = cookie.split('=');
+        console.log(tabValue);
+        if(tabValue[0] === name){
+            return tabValue[1];
+        }
+    }
+}
